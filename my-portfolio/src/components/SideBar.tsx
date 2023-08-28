a90b8124278d41ec774c3895e7b80e1d195f6670
Ronnie <nicandro.ronnie@gmail.com>
2023-08-28 19:33:40 +0800

    updated link socials

--- my-portfolio/src/components/SideBar.tsx
+++ my-portfolio/src/components/SideBar.tsx
@@ -6,25 +6,26 @@ const SideBar = () => {
     <div className="hidden md:flex fixed flex-col top-[35%] left-[0%] ">
       <ul className="flex flex-col">
         <li className="w-[160px] ml-[-100px] hover:ml-[1px]  duration-300 bg-gray-800 px-15 py-2 rounded-sm">
-          <a href="/" className="flex justify-between items-center ">
+          <a href="https://www.facebook.com/ssisjqjusjdnqnsjoqo" className="flex justify-between items-center" target='_blank'>
             Facebook <FaFacebook size={30} />
           </a>
         </li>
         <li className="w-[160px] ml-[-100px] hover:ml-[1px]  duration-300 bg-[#333333] px-15 py-2 rounded-sm">
-          <a href="/" className="flex justify-between items-center">
+          <a href="https://github.com/ginlord111" className="flex justify-between items-center" target='_blank'>
             Github <FaGithub size={30} />
           </a>
         </li>
         <li className="w-[160px] ml-[-100px] hover:ml-[1px]  bg-blue-600  duration-300 px-15 py-2 rounded-sm">
           <a
-            href="/"
+            href="https://linkedin.com/in/ronnie-nicandro-7a3191246"
             className="flex justify-between items-center"
+            target='_blank'
           >
             LinkedIn <FaLinkedin size={30} />
           </a>
         </li>
         <li className="w-[160px] ml-[-100px] hover:ml-[1px] bg-[#565f69] duration-300 px-15 py-2 rounded-sm">
-          <a href="/" className="flex justify-between items-center">
+          <a href="https://drive.google.com/file/d/12whcXtQgkduZOqXEu77NzbExUrdWfQFb/view?usp=sharing" target='_blank' className="flex justify-between items-center">
             Resume <BsFillPersonLinesFill size={30} />
           </a>
         </li>
